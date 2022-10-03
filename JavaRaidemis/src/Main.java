904fd23fbc5d469dd7d1f0958b366d53fe1bc612
Mantas Povilas Bagociunas <mpovilasb@live.com>
2022-10-03 22:46:25 +0300

    Initial commit

---
+++ JavaRaidemis/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print(" J\t\t\t A\t\t V\t   V \t  A\n J \t \t\tA A\t\t  V\t  V\t\t" +
+                " A A\n J\t\t   A A A\t   V V\t\tA A A\nJJ\t\t  A\t    A\t\tV\t   A     A");
+
+
+
+    }
+}
---
+++ Lentele/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("a\t\ta^2\t\ta^3\t\ta^4");
+        System.out.println("---------------------------");
+        System.out.println("1\t\t1\t\t1\t\t1");
+        System.out.println("2\t    "+(2*2)+"\t    "+(2*2*2)+"\t    "+(2*2*2*2));
+        System.out.println("3\t    "+(3*3)+"\t    "+(3*3*3)+"\t    "+(3*3*3*3));;
+        System.out.println("4\t    "+(4*4)+"\t    "+(4*4*4)+"\t    "+(4*4*4*4));;
+    }
+}
