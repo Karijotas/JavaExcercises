58c9b9859103af9a55f11f7974c9cf008b76935b
Mantas Povilas Bagociunas <mpovilasb@live.com>
2022-10-07 14:53:27 +0300

    unfinished

---
+++ Pratimas12/Pazymiai2/src/lt/techin/mantaspovilas/pazymiai/Pazymiai.java
@@ -0,0 +1,29 @@
+package lt.techin.mantaspovilas.pazymiai;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Pazymiai {
+    public static void main(String[] args) {
+        int pazymSkaic = 0;
+        int vien = 0;
+        int ket = 0;
+        int pen = 0;
+        int ses = 0;
+        int sept = 0;
+        int ast = 0;
+        int dev = 0;
+        int[] pazymiai = {1, 5, 8, 7, 10, 9, 9, 8, 7, 7, 4, 6, 8};
+
+        pazymSkaic = (int) Arrays.stream(pazymiai).count();
+        int c = 0;
+        int i = 0;
+        while(c<pazymiai.length){
+
+         System.out.println();
+         c++;
+
+        }
+        System.out.println("8- " );
+    }
+}
