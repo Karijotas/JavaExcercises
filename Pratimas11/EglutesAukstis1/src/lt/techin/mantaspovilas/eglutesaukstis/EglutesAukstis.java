03d5036cf7284613e725ad857f1e7bf3f183ddc7
Mantas Povilas Bagociunas <mpovilasb@live.com>
2022-10-07 11:55:06 +0300

    1 pratimas

---
+++ Pratimas11/EglutesAukstis1/src/lt/techin/mantaspovilas/eglutesaukstis/EglutesAukstis.java
@@ -0,0 +1,30 @@
+package lt.techin.mantaspovilas.eglutesaukstis;
+import java.util.Arrays;
+import java.util.Scanner;
+public class EglutesAukstis {
+    public static void main(String[] args) {
+        Scanner scanner  = new Scanner(System.in);
+            double vidurkis =0.0;
+            int i = 1;
+            int counter = 0;
+            System.out.println("Kiek egluciu atvezta? ");
+            double kiekis = Double.valueOf(scanner.nextLine());
+            double[] eglutes = new double[(int)kiekis];
+            while(i<=kiekis){
+                System.out.println("Iveskite "+i+" eglutes auksti:");
+
+                eglutes[counter] = Double.valueOf(scanner.nextLine());
+                ++i;
+                ++counter;
+            }
+
+            vidurkis = Arrays.stream(eglutes).sum() / eglutes.length;
+
+            System.out.println("Eglutes aukscio vidurkis: "+ vidurkis +" cm");
+
+
+
+
+
+    }
+}
