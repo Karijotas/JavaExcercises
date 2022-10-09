dd178e8274ea20fec3d2b51d0d32c44654a062e2
Mantas Povilas Bagociunas <mpovilasb@live.com>
2022-10-09 16:45:46 +0300

    Excercise12-2 finished

--- Pratimas12/Pazymiai2/src/lt/techin/mantaspovilas/pazymiai/Pazymiai.java
+++ Pratimas12/Pazymiai2/src/lt/techin/mantaspovilas/pazymiai/Pazymiai.java
@@ -1,29 +1,45 @@
 package lt.techin.mantaspovilas.pazymiai;
 
 import java.lang.reflect.Array;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 
 public class Pazymiai {
+    private static DecimalFormat df = new DecimalFormat("#.##");
     public static void main(String[] args) {
-        int pazymSkaic = 0;
-        int vien = 0;
-        int ket = 0;
-        int pen = 0;
-        int ses = 0;
-        int sept = 0;
-        int ast = 0;
-        int dev = 0;
+        int i = 1;
         int[] pazymiai = {1, 5, 8, 7, 10, 9, 9, 8, 7, 7, 4, 6, 8};
 
-        pazymSkaic = (int) Arrays.stream(pazymiai).count();
-        int c = 0;
-        int i = 0;
-        while(c<pazymiai.length){
 
-         System.out.println();
-         c++;
 
+        while ( i <= 10) {
+            System.out.println(i+"- "+ df.format(getPercentageOfGrade(pazymiai, i))+"%");
+            ++i;
         }
-        System.out.println("8- " );
+
     }
+
+    private static double getPercentageOfGrade(int[] arr, int i) {
+        double percentage = 0.0;
+        double pazymSkaic = 0;
+        pazymSkaic = Arrays.stream(arr).count();
+
+        percentage = (getCountOfValueFromArray(arr, i) * 100) / pazymSkaic;
+        return percentage;
+
+
+    }
+
+    private static int getCountOfValueFromArray(int[] arr, int n) {
+        int count = 0;
+      for (int each : arr) {
+          if (each == n){
+              ++count;
+          }
+      }
+      return count;
+    }
+
 }
+
+
