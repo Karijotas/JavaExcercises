443245225e6c52b6533e10f043e204437bdfecd3
Mantas Povilas Bagociunas <mpovilasb@live.com>
2022-11-13 19:23:44 +0200

    started

---
+++ Pratimas15/SimpleCalculator/src/lt/techin/mantaspovilas/simplecalculator/TextUI.java
@@ -0,0 +1,18 @@
+package lt.techin.mantaspovilas.simplecalculator;
+
+public class TextUI {
+    public static void main(String[] args) {
+
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t===Welcome to Simple Calculator===\t\t\t\t\t\t\t\t\t\t\t\t");
+        System.out.println();
+        System.out.println("\t\t\t\t\t\t\t\t\t\tChoose which kind of operation you want to make: ");
+        System.out.println();
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t1 # Arithmetic : +,-,*,/\t\t\t\t\t\t\t\t\t\t\t\t");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t2 # Sine, cosine, tangent\t\t\t\t\t\t\t\t\t\t\t\t");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t3 # Percentage \t\t\t\t\t\t\t\t\t\t\t\t");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t4 # Square root \t\t\t\t\t\t\t\t\t\t\t\t");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t5 # Exponentiation \t\t\t\t\t\t\t\t\t\t\t\t");
+
+
+    }
+}
